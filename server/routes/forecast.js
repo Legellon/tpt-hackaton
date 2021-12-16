53d985c759548e8c90aea807adc41998d2fd1411
Nikita Rehemae <nikita.rehemae@gmail.com>
2021-12-16 13:18:02 +0200

    Some changes

--- server/routes/forecast.js
+++ server/routes/forecast.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: "Forecast Main Page"
-    })
+router.get('/', async (req, res) => {
+    const currentTime = new Date(1639605600 * 1000);
+
+    console.log(currentTime);
+
+    axios.get('https://dashboard.elering.ee/api/nps/price?start=2021-12-15T22%3A00%3A00.000Z&end=2021-12-16T21%3A00%3A00.000Z')
+        .then((result) => {
+            const forecast = result.data.data.ee;
+            const array = [forecast];
+
+            array.sort((a, b) => a.price - b.price)
+ 
+            console.log(array)
+        }).catch((err) => {
+            console.log(err);
+        });
 });
 
 module.exports = router;
