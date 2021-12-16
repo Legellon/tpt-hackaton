26a998eae2dd6839875f48a0c26047d800fb5210
Nikita Rehemae <nikita.rehemae@gmail.com>
2021-12-16 13:53:58 +0200

    Some changes

--- server/routes/forecast.js
+++ server/routes/forecast.js
@@ -11,13 +11,29 @@ router.get('/', async (req, res) => {
     axios.get('https://dashboard.elering.ee/api/nps/price?start=2021-12-15T22%3A00%3A00.000Z&end=2021-12-16T21%3A00%3A00.000Z')
         .then((result) => {
             const forecast = result.data.data.ee;
-            const array = [forecast];
+            const originForecast = [...forecast];
 
-            array.sort((a, b) => a.price - b.price)
- 
-            console.log(array)
+            forecast.sort((a, b) => a.price - b.price);
+
+            const cheapHours = []
+            const expensiveHours = []
+
+            for (let i = 1; i <= 4; i++) {
+                cheapHours.push(forecast[i - 1]);
+                expensiveHours.push(forecast[forecast.length - i]);
+            }
+
+            res.json({
+                status: true,
+                cheaps: cheapHours,
+                expensives: expensiveHours,
+                forecast: originForecast
+            });
         }).catch((err) => {
-            console.log(err);
+            res.json({
+                status: false,
+                message: 'API connection failed'
+            });
         });
 });
 
